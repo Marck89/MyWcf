c199ca94b18a8b4ec6e904b7ed7896954b75432d
Marcello FABBRI <marcello.fabbri@alten.it>
2022-03-17 16:42:19 +0100

    first commit

---
+++ MyWcf/CalcService.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceModel;
+namespace Calc
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class CalcService : ICalcService
+    {
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+        public double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
---
+++ MyWcf/ICalcService.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+namespace Calc
+{
+    [ServiceContract]
+    public interface ICalcService
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
